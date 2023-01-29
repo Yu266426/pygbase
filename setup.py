6d1f3f691428876c23bd5e3c2f125d61a611ec9c
Yu266426 <yu266426@gmail.com>
2023-01-29 14:55:32 -0800

    Update

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
 	name="pygbase",
 	version="0.1",
 	description="A basic engine for Pygame to help with resource loading, and handling of game states",
-	
+	url="https://github.com/Yu266426/pygbase",
+	author="Yu266426",
+	author_email="yu266426@gmail.com",
+	license="MIT",
+	packages=["pygbase"],
+	install_requires=["pygame"]
 )
