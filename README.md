f476290533c1c33bb7e3df46d25d4b2550616fa6
Yu266426 <yu266426@gmail.com>
2023-03-06 19:04:19 -0800

    Added PyPi setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# `pygbase-engine`
+
+`pygbase-engine` is a simple engine for pygame that handles states, ui, resources, and more.
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-	name="pygbase",
-	version="0.1",
+	name="pygbase-engine",
+	version="0.0.1",
 	description="A basic engine for Pygame to help with resource loading, and handling of game states",
 	url="https://github.com/Yu266426/pygbase",
 	author="Yu266426",
 	author_email="yu266426@gmail.com",
 	license="MIT",
 	packages=["pygbase"],
-	install_requires=["pygame"]
+	install_requires=["pygame-ce"]
 )
