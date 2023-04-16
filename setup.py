c6bf4b9538ea3342e7f336de18d186b435b1052e
Yu266426 <yu266426@gmail.com>
2023-04-16 07:34:39 -0700

    Test Fix 2

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
 	author="Yu266426",
 	author_email="yu266426@gmail.com",
 	license="MIT",
-	packages=["pygbase"],
+	packages=["pygbase", "pygbase.graphics", "pygbase.particles", "pygbase.ui"],
 	install_requires=["pygame-ce"]
 )
