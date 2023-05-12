b659e48f668d6be58fe7b3904fba3b75c45071ef
Yu266426 <yu266426@gmail.com>
2023-05-11 21:04:17 -0700

    Fixed inconsistancy

--- pygbase/ui/ui_manager.py
+++ pygbase/ui/ui_manager.py
@@ -17,7 +17,7 @@ class UIManager:
 		self._frames.append(frame)
 		return frame
 
-	def add_element(self, element: UIElement, align_with_previous: tuple = (False, False), add_on_to_previous: tuple = (False, False)) -> Frame:
+	def add_element(self, element: UIElement, align_with_previous: tuple = (False, False), add_on_to_previous: tuple = (False, False)) -> UIElement:
 		return self._frames[0].add_element(element, align_with_previous=align_with_previous, add_on_to_previous=add_on_to_previous)
 
 	def on_ui(self) -> bool:
