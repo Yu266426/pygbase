90bc4064a68171e3888bcba1bce17b6afec6e718
Yu266426 <yu266426@gmail.com>
2024-03-30 12:48:35 -0700

    Hopefully fix angled image crash

--- pygbase/graphics/image.py
+++ pygbase/graphics/image.py
@@ -53,6 +53,7 @@ class Image:
 		angle %= 360
 
 		image_index = int(angle / self.rotate_angle)
+		image_index = min(max(image_index, 0), len(self.angled_images))
 		return self.angled_images[image_index]
 
 	def draw(self, surface: pygame.Surface, pos: pygame.Vector2 | tuple[float, float], angle: float = 0, flip: tuple[bool, bool] = (False, False), draw_pos: str = "topleft", flags: int = 0):
